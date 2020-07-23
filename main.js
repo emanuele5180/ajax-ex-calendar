7c72b48070124a9bec94dfb9e7ad88aecffcbb75
emanuele5180 <macina5180>
2020-07-23 15:43:22 +0200

    gennaio

---
+++ main.js
@@ -0,0 +1,39 @@
+// $(document).ready(function() {
+// 	//Code
+// });
+
+
+// Descrizione:
+// Creiamo un calendario dinamico con le festività.
+// Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull'API).
+// Milestone 1
+// Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
+// Milestone 2
+// Diamo la possibilità di cambiare mese, gestendo il caso in cui l'API non possa ritornare festività.
+// Attenzione!
+// Ogni volta che cambio mese dovrò:
+// Controllare se il mese è valido (per ovviare al problema che l'API non carichi holiday non del 2018)
+// Controllare quanti giorni ha il mese scelto formando così una lista
+// Chiedere all'api quali sono le festività per il mese scelto
+// Evidenziare le festività nella lista
+// Consigli e domande del giorno:
+// Abbiamo visto assieme una libereria che serve per gestire le date... quale sarà?
+// Una chiamata ajax può anche non andare a buon fine, che si fa in quel caso? Lasciamo l'utente ad attendere? ;)
+
+//API: https://flynn.boolean.careers/exercises/api/holidays
+
+
+
+// Milestone 1
+// Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
+
+function init() {
+
+  console.log("hello world");
+
+
+
+
+}
+
+$(document).ready(init);
