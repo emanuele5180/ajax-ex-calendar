a82584f1c4b9c3970553b0118e238bad2ddd3e1d
emanuele5180 <macina5180>
2020-07-24 10:49:03 +0200

    milestone 1

--- main.js
+++ main.js
@@ -27,13 +27,96 @@
 // Milestone 1
 // Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
 
-function init() {
+function printMonth(currentMonth) {
+  var daysInMonth = currentMonth.daysInMonth();
+  var template = $("#template").html();
+  var compiled = Handlebars.compile(template);
+  var target = $(".giorni-mese");
+  target.html("");
+  for (var i = 1; i <= daysInMonth; i++) {
+    var datecomplete = moment({ year :currentMonth.year(), month :currentMonth.month(), day :i});
+    var dayshtml = compiled({
+      "value": i,
+      "datecomplete": datecomplete.format("YYYY-MM-DD")
+    });
+    target.append(dayshtml);
+  }
+}
 
-  console.log("hello world");
+function printHoliday(currentMonth) {
+  var year = currentMonth.year();
+  var month = currentMonth.month();
+  $.ajax({
+  url: "https://flynn.boolean.careers/exercises/api/holidays",
+  method: "GET",
+  data: {
+    "year": year,
+    "month": month
+  },
 
+  success: function (data, state) {
+    var holidays = data["response"];
+    for (var i = 0; i < holidays.length; i++) {
+      var element = $(".giorni-mese li[data-datecomplete='"+holidays[i]["date"]+"']")
+      element.addClass("holidays");
+      element.append("  " + holidays[i]["name"]);
+    }
 
+  },
+  error: function  (error) {
+
+  }
+});
+
+}
+
+function changeMonth(){
+  var btn = $('button #prev ,button #next');
+  btn.click(getNewMonth);
+  console.log(btn);
+  
 
 
 }
 
+function getNewMonth(){
+  var monthSwap = $(this);
+  console.log(monthSwap);
+
+}
+
+function init() {
+  var currentMonth = moment("2018-01-01");
+  //console.log(currentMonth.month());
+  printMonth(currentMonth);
+  printHoliday(currentMonth);
+
+  changeMonth()
+
+
+}
+
+
 $(document).ready(init);
+
+// function printMonth(currentMonth){
+//   var daysInMonth = currentMonth.daysInMonth();
+//
+//   for (var i = 1; i <= daysInMonth; i++) {
+//     array[i]
+//   }
+// }
+// function init() {
+//
+//   var currentMonth = moment("2018-01-01");
+//   console.log(currentMonth);
+//   printMonth(currentMonth);
+//
+//   console.log("hello world");
+//
+//
+//
+//
+// }
+//
+// $(document).ready(init);
